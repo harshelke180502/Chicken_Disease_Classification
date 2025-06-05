cc8868b6bd4436d217ad96ad7630893f2fe79b6e
Harsh Shelke <harsh_shelke@Harshs-MacBook-Air.local>
2025-06-04 19:24:30 -0500

    data ingestion completed

---
+++ main.py
@@ -0,0 +1,12 @@
+from Chicken_Disease_Classifier import logger
+from Chicken_Disease_Classifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+STAGE_NAME="Data Ingestion Stage"
+
+try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        data_ingestion = DataIngestionTrainingPipeline()
+        data_ingestion.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
